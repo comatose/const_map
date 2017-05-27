0742aeec4ef0ecf54b0b39c4869cc3625ff60c2a
comatose <boncheol.gu@gmail.com>
2017-05-27 23:26:20 +0900

    Added dependency on Hana.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
-rm -rf build
+#!/usr/bin/env sh
+
+rm -rf build ext
 mkdir build
 cmake -H. -Bbuild -DCMAKE_CXX_INCLUDE_WHAT_YOU_USE=iwyu -DCMAKE_CXX_COMPILER=clang++
 # cmake -H. -Bbuild -DCMAKE_CXX_INCLUDE_WHAT_YOU_USE=iwyu -DCMAKE_CXX_CLANG_TIDY=clang-tidy
 # cmake --build build -- VERBOSE=1
-cmake --build build
+# cmake --build build
